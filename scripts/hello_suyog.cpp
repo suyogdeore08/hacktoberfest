f71e5582c7f89cb7bf5f6d69b5199aa9e67ed1f0
suyogdeore08 <suyogdeore08@gmail.com>
2025-10-02 10:33:14 +0530

    Created a hello_suyog.cpp
    A basic program that runs "Hello World".

---
+++ scripts/hello_suyog.cpp
@@ -0,0 +1,11 @@
+// LANGUAGE: CPP/C++
+// AUTHOR: Suyog Deore
+// GITHUB: https://github.com/suyogdeore08
+
+#include<iostream>
+using namespace std;
+
+int main(){
+cout<<"Hello World!";
+return 0;
+}
